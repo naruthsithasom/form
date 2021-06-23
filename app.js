ef4a08ee548006b387b47645941d6810415a688b
naruthsithasom <ett0088@gmail.com>
2021-06-23 09:08:58 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // npm install express ejs body-parser cookie-parser mysql multer
 const express = require('express')
 const app = express()
-const mysql   = require('mysql')
-const readBody = express.urlencoded({extended:false})
+const mysql = require('mysql')
+const readBody = express.urlencoded({ extended: false })
 const AWS = require('aws-sdk');
 const dotenv = require('dotenv');
+const { render } = require('ejs');
 
 dotenv.config();
 
@@ -14,81 +15,123 @@ const kmsClient = new AWS.KMS({
   secretAccessKey: "foAjEvEkmVLTSqYIY5h/HZ2IwIsz+oc6T9ye1P8U"
 });
 
-var pool    = mysql.createPool({ host:'54.251.234.220:3306', database:'db_kbtg', user:'root', password:'Ypqe=LyiQ6ti'})
+const pool = mysql.createPool({ host: 'localhost', database: 'db_kbtg', user: 'root', password: 'Ypqe=LyiQ6ti'})
+
 app.listen(8080)
 app.engine('html', require('ejs').renderFile)
-app.get('/jobs', (req, res) => res.render('index.html'))
-app.post('/jobs', readBody, saveJobs)
+app.get('/add', showIndex)
+app.post('/add', readBody, addMember)
+app.get('/member', showMember)
 
-app.get('/register', (req, res) =>  (pool.query(`select * from register`, ( error,data ) => { 
-     data.forEach((value, key) => {
-       console.log(`key: ${key} : value: ${value}`)
-     });
-    res.send(data) 
+function showIndex(req, res){
+  res.render('index.html')
+}
+
+function showMember(req, res){
+  pool.query(`select * from register`, async function (error, data) {
+
+    let dataDecode = []
+    let store = []
+    for (let i = 1; i < data.length; i++) {
+      dataDecode.push(data[i].full_name)
+      dataDecode.push(data[i].email)
+      dataDecode.push(data[i].linkedin)
+      dataDecode.push(data[i].messages)
+    }
+  
+    let j = 0
+    while (j < dataDecode.length) {
+      
+      let decryptMySecureText = await decryptEncodedstring(dataDecode[j]);
+      store.push(decryptMySecureText)
+      console.log("\n\decryptResult: " + decryptMySecureText);
+      
+      j++;
+    }
+    res.send(store)
+  
   })
-))
+}
+/*
+app.get('/member', (req, res) => (pool.query(`select * from register`, async function (error, data) {
 
-async function  saveJobs(req, res){
+  let dataDecode = []
 
-	let sql = 'insert into `register`(full_name, email, linkedin, messages) values (?, ?, ?, ?)'
-	let data = [req.body.full_name, req.body.email, req.body.linkedin, req.body.messages]
+  for (let i = 1; i < data.length; i++) {
+    dataDecode.push(data[i].full_name)
+    dataDecode.push(data[i].email)
+    dataDecode.push(data[i].linkedin)
+    dataDecode.push(data[i].messages)
+  }
+
+  let j = 0
+  while (j < dataDecode.length) {
+    
+    let decryptMySecureText = await decryptEncodedstring(dataDecode[j]);
+    console.log("\n\decryptResult string : " + decryptMySecureText);
+    
+    j++;
+  }
+  res.send(data)
+
+})))
+*/
+async function addMember(req, res) {
+
+  let sql = 'insert into `register`(full_name, email, linkedin, messages) values (?, ?, ?, ?)'
+  let data = [req.body.full_name, req.body.email, req.body.linkedin, req.body.messages]
   let dataEncode = []
 
-  // res.send('welcome <br> ' + data )
   let i = 0
-   
-  while( i < data.length ){
-    
-    let encryptMySecureText = await encryptString(data[i]);  
-    
+
+  while (i < data.length) {
+
+    let encryptMySecureText = await encryptString(data[i]);
+
     dataEncode[i] = encryptMySecureText
-    
+
     console.log("\n\nEncrypted string : " + encryptMySecureText);
-    
-    // let decryptMySecureText = await decryptEncodedstring(encryptMySecureText);
-    
-    // console.log("\n\decryptResult string : " + decryptMySecureText);
-    
+
     i++;
-   }
-  //  console.log(dataEncode)
-  pool.query(sql, dataEncode, function(error, result){
-		let model = { }
-		if(error == null){
+  }
+  pool.query(sql, dataEncode, function (error, result) {
+    let model = {}
+    if (error == null) {
 
-			model.message = 'Register Success...'
+      model.message = 'Register Success...'
 
-		} else {
+    } else {
 
-			model.message = 'Fail to register...'
-		}
-	})
-  
+      model.message = 'Fail to register...'
+    }
+  })
+
 }
 async function encryptString(text) {
 
   const paramsEncrypt = {
-      KeyId: 'arn:aws:kms:ap-southeast-1:538613827427:key/f7ea6177-c4db-4537-9528-00573d56de69',
-      Plaintext: new Buffer.from(text)
+    KeyId: 'arn:aws:kms:ap-southeast-1:538613827427:key/f7ea6177-c4db-4537-9528-00573d56de69',
+    Plaintext: new Buffer.from(text)
   };
 
   const encryptResult = await kmsClient.encrypt(paramsEncrypt).promise();
   if (Buffer.isBuffer(encryptResult.CiphertextBlob)) {
-      return Buffer.from(encryptResult.CiphertextBlob).toString('base64');
+    return Buffer.from(encryptResult.CiphertextBlob).toString('base64');
   } else {
-      throw new Error('error');
+    throw new Error('error');
   }
 }
+
 async function decryptEncodedstring(encoded) {
- 
+
   const paramsDecrypt = {
-      CiphertextBlob: Buffer.from(encoded, 'base64')
+    CiphertextBlob: Buffer.from(encoded, 'base64')
   };
 
   const decryptResult = await kmsClient.decrypt(paramsDecrypt).promise();
   if (Buffer.isBuffer(decryptResult.Plaintext)) {
-      return Buffer.from(decryptResult.Plaintext).toString();
+    return Buffer.from(decryptResult.Plaintext).toString();
   } else {
-      throw new Error('error');
+    throw new Error('error');
   }
 }
